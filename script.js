6bb9dbbe6cf387f1f2cef6ef96f199ec266449d8
Kurtmeisterino <kurdo_rodin@hotmail.com>
2024-08-28 01:54:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Här kan du lägga till interaktivitet senare, om det behövs.
 });
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    if (window.pageYOffset > 100) {
+        header.classList.add('shrink');
+    } else {
+        header.classList.remove('shrink');
+    }
+};
